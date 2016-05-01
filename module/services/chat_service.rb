2df824972e18eaf657ba1609b18d2a6bb92f80bb
Evan Waters <evan.waters@gmail.com>
2016-05-01 15:15:28 -0700

    Including person name with chat.

--- module/services/chat_service.rb
+++ module/services/chat_service.rb
@@ -16,7 +16,7 @@ module Gabb
       people = Gabb::Person.where(:device_token.ne => nil)
       notifications = []
       people.each do |person|
-        notifications << APNS::Notification.new(person.device_token, alert: chat.text, badge: nil, other: { chat_id: chat.id })
+        notifications << APNS::Notification.new(person.device_token, alert: "#{chat.person.full_name}: #{chat.text}", badge: nil, other: { chat_id: chat.id })
       end
       APNS.send_notifications notifications
     end
